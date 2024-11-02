9cb31f20b341877c933ff5370e6a5f5e2f54eac4
Anders Kaseorg <andersk@mit.edu>
2024-11-01 18:36:59 -0700

    Fix pickling of httpclient.HTTPError subclasses and web.HTTPError
    The `args` member variable is set by `BaseException.__new__` and used
    by `BaseException.__reduce__` for pickling.  To avoid interfering with
    it, we need to avoid calling `BaseException.__init__` from classes
    that have subclasses with incompatible constructors, and rename our
    own `tornado.web.HTTPError.args` member.
    
        >>> pickle.loads(pickle.dumps(tornado.simple_httpclient.HTTPTimeoutError("message")))
        Traceback (most recent call last):
          File "<stdin>", line 1, in <module>
        TypeError: HTTPTimeoutError.__init__() takes 2 positional arguments but 4 were given
        >>> str(pickle.loads(pickle.dumps(tornado.web.HTTPError(500, "%s", "foo"))))
        Traceback (most recent call last):
          File "<stdin>", line 1, in <module>
          File "/home/anders/python/tornado/tornado/web.py", line 2488, in __str__
            return message + " (" + (self.log_message % self.args) + ")"
                                     ~~~~~~~~~~~~~~~~~^~~~~~~~~~~
        TypeError: not enough arguments for format string
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- tornado/web.py
+++ tornado/web.py
@@ -1869,7 +1869,9 @@ class RequestHandler(object):
         if isinstance(value, HTTPError):
             if value.log_message:
                 format = "%d %s: " + value.log_message
-                args = [value.status_code, self._request_summary()] + list(value.args)
+                args = [value.status_code, self._request_summary()] + list(
+                    value.log_args
+                )
                 gen_log.warning(format, *args)
         else:
             app_log.error(
@@ -2474,7 +2476,7 @@ class HTTPError(Exception):
     ) -> None:
         self.status_code = status_code
         self.log_message = log_message
-        self.args = args
+        self.log_args = args
         self.reason = kwargs.get("reason", None)
         if log_message and not args:
             self.log_message = log_message.replace("%", "%%")
@@ -2485,7 +2487,7 @@ class HTTPError(Exception):
             self.reason or httputil.responses.get(self.status_code, "Unknown"),
         )
         if self.log_message:
-            return message + " (" + (self.log_message % self.args) + ")"
+            return message + " (" + (self.log_message % self.log_args) + ")"
         else:
             return message
 
